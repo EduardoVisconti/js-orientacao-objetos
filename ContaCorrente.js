8ccc4b5a6fe948b342f3e105b5ee1c9d5f6832c1
Eduardo Visconti <duduhsg@gmail.com>
2022-01-12 11:54:25 -0800

    done

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0; //Sempre que criar uma nova conta, não seria criado um novo total, mantendo o valor correto.
     agencia;
     _cliente;
      // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
@@ -24,6 +25,7 @@ export class ContaCorrente{
     constructor (agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor){
--- index.js
+++ index.js
@@ -4,18 +4,13 @@ import {ContaCorrente} from "./ContaCorrente.js";
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
-console.log(cliente2.cpf);
 
-cliente2._cpf = -1;
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-
 const conta2 = new ContaCorrente(102, cliente2);
-//conta2.cliente = cliente1;
-//conta2.agencia = 102;
 
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
+console.log(ContaCorrente.numeroDeContas);
